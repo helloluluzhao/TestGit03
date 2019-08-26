677044391378b782877e901cee4a57cf4992121f
helloluluzhao <michaelscofield38@yahoo.com>
2019-08-26 21:17:20 +0800

    TestGit01第一次提交

---
+++ TestGit03/src/main/java/com/atguigu/Apple.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class Apple {
+	public static void main(String[] args) {
+
+		System.out.println("right");
+		System.out.println("right");
+		System.out.println("right");
+		System.out.println("right");
+
+	}
+}
---
+++ TestGit03/src/main/java/com/atguigu/Happy.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class Happy {
+
+	public static void main(String[] args) {
+		System.out.println("test ... ");
+	}
+
+}
